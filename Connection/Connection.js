94f22ac5be90127405dc5e12e6dec554a294563e
Muhammad Subhan Hussnain <ssm156789@gmail.com>
2025-05-12 16:58:56 +0500

    Update vercel.json

--- Connection/Connection.js
+++ Connection/Connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ConnectToDatabase = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ssm156789:SUBH%40n1122@cluster0.lohe7vl.mongodb.net/todolist?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
