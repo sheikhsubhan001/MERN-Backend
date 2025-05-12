b935ddaaa7c480f0d30a1cce76c946698988e402
Muhammad Subhan Hussnain <ssm156789@gmail.com>
2025-05-12 17:30:17 +0500

    Update Connection.js

--- Connection/Connection.js
+++ Connection/Connection.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const ConnectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI
+    //     , {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
+);
 
     console.log('Database Connected Successfully');
   } catch (err) {
